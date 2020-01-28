6f9dcb5b1fc5db0b2bc401af094e15c976ceb278
Matthew Harris <matthew.harris@ncr.com>
2020-01-28 14:48:27 -0500

    showing list of todos on android

--- app/src/main/java/sample/SampleAndroid.kt
+++ app/src/main/java/sample/SampleAndroid.kt
@@ -1,22 +1,30 @@
 package sample
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import arch.AndroidCommonData
 import arch.ResourceView
 import arch.ResourceViewObserver
 import arch.android
 import business.TodoListHolder
 import business.TodosViewModel
+import kotlinx.android.synthetic.main.activity_main.*
+import model.Todo
 
 actual class Sample {
     actual fun checkMe() = 44
 }
 
 actual object Platform {
-    actual val name: String = "Androids"
+    actual val name: String = "Android"
 }
 
 class MainActivity : AppCompatActivity() {
@@ -28,26 +36,47 @@ class MainActivity : AppCompatActivity() {
 
         Sample().checkMe()
         setContentView(R.layout.activity_main)
-        findViewById<TextView>(R.id.main_text).text = hello()
 
         viewModel.todoData.android().data.observe(this, ResourceViewObserver(todoView))
         viewModel.loadTodos()
 
+        rvTodos.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        rvTodos.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+
+        supportActionBar?.title = hello()
     }
 
-    val todoView = object: ResourceView<TodoListHolder> {
+    private val todoView = object: ResourceView<TodoListHolder> {
         override fun showData(data: TodoListHolder) {
-            Toast.makeText(this@MainActivity, "loaded  ${data.todos.size} todos", Toast.LENGTH_LONG).show()
+            rvTodos.adapter = TodoAdapter(data.todos)
         }
 
         override fun showLoading(isLoading: Boolean) {
-            if (isLoading) {
-                Toast.makeText(this@MainActivity, "loading todos", Toast.LENGTH_LONG).show()
-            }
+            todoProgress.visibility = if (isLoading) View.VISIBLE else View.GONE
         }
 
         override fun showError(error: Throwable) {
             Toast.makeText(this@MainActivity, "loading todos failed: ${error.message}", Toast.LENGTH_LONG).show()
         }
+    }
+}
+
+class TodoAdapter(private val todos: List<Todo>): RecyclerView.Adapter<TodoViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.view_todo_item, parent, false)
+        return TodoViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = todos.size
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int)  = holder.bind(todos[position])
+}
+
+class TodoViewHolder(itemView: View): RecyclerView.ViewHolder (itemView) {
+
+    private val txtTitle = itemView.findViewById<TextView>(R.id.view_todo_item)
+
+    fun bind(todo: Todo) {
+        txtTitle.text = todo.title
     }
 }
